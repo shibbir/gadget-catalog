158977471daf7bf569b71aeb1a2b5bf8ed056555
Shibbir Ahmed <shibbir.io@outlook.com>
2020-05-17 00:35:30 +0600

    Upgrade brand-form to functional component

--- src/modules/brand/client/brand-form.component.js
+++ src/modules/brand/client/brand-form.component.js
@@ -1,85 +1,56 @@
-import React from "react";
-import { connect } from "react-redux";
-import { Form, withFormik } from "formik";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Form, Formik } from "formik";
 import { Divider, Button } from "semantic-ui-react";
 import BrandSchema from "./brand.schema";
 import { createBrand, updateBrand, getBrand } from "./brand.actions";
 import { TextInput } from "../../core/client/components/FieldInput/FieldInputs";
 
-class BrandForm extends React.Component {
-    constructor(props) {
-        super();
+export default function BrandForm({id}=props) {
+    const dispatch = useDispatch();
 
-        if(props.brandId) {
-            props.getBrand(props.brandId);
-        }
+    if(id) {
+        useEffect(() => {
+            dispatch(getBrand(id));
+        }, []);
     }
 
-    render() {
-        const { handleSubmit, isSubmitting } = this.props;
+    const brand = useSelector(state => state.brandReducer.brand);
 
-        return (
-            <div>
-                <Form onSubmit={handleSubmit} className="ui form">
-                    <TextInput attributes={{
-                        type: "text",
-                        name: "name",
-                        label: "Name",
-                        required: true
-                    }}/>
-                    <Divider hidden/>
-                    <Button type="submit" positive disabled={isSubmitting}>Submit</Button>
-                    <Button type="reset" disabled={isSubmitting}>Reset</Button>
-                </Form>
-            </div>
-        );
-    }
+    return (
+        <div>
+            <Formik
+                initialValues={{
+                    name: id && brand ? brand.name : ""
+                }}
+                displayName="BrandForm"
+                enableReinitialize={true}
+                validationSchema={BrandSchema}
+                onSubmit={(values, actions) => {
+                    if(id) {
+                        dispatch(updateBrand(values, id));
+                    } else {
+                        dispatch(createBrand(values));
+                        actions.resetForm();
+                    }
+
+                    actions.setSubmitting(false);
+                }}
+            >
+                {props => (
+                    <Form onSubmit={props.handleSubmit} className="ui form">
+                        <TextInput attributes={{
+                            type: "text",
+                            name: "name",
+                            label: "Name",
+                            required: true
+                        }}/>
+                        <Divider hidden/>
+                        <Button type="submit" positive disabled={props.isSubmitting}>Submit</Button>
+                        <Button type="reset" disabled={props.isSubmitting}>Reset</Button>
+                    </Form>
+                )}
+            </Formik>
+        </div>
+    );
 }
-
-BrandForm = withFormik({
-    enableReinitialize: true,
-
-    mapPropsToValues: props => {
-        return {
-            name: props.brandId && props.brand ? props.brand.name : ""
-        };
-    },
-
-    validationSchema: BrandSchema,
-
-    handleSubmit: (formValues, { setSubmitting, resetForm, props }) => {
-        if(props.brandId) {
-            props.updateBrand(formValues, props.brandId);
-        } else {
-            props.createBrand(formValues);
-            resetForm();
-        }
-
-        setSubmitting(false);
-    },
-
-    displayName: "BrandForm"
-})(BrandForm);
-
-const mapStateToProps = (state, props) => {
-    return {
-        brandId: props.id,
-        brand: state.brandReducer.brand
-    };
-};
-
-const mapDispatchToProps = (dispatch) => {
-    return {
-        createBrand: (formData) => {
-            dispatch(createBrand(formData));
-        },
-        updateBrand: (formData, itemId) => {
-            dispatch(updateBrand(formData, itemId));
-        },
-        getBrand: (itemId) => {
-            dispatch(getBrand(itemId));
-        }
-    };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(BrandForm);
