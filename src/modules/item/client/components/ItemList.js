0e943dd066c5717367e0efb99c6a36ce4c828685
Shibbir Ahmed <shibbir.io@outlook.com>
2020-06-01 01:53:05 +0600

    Update ItemList component to functional component

--- src/modules/item/client/components/ItemList.js
+++ src/modules/item/client/components/ItemList.js
@@ -1,270 +1,258 @@
-import React from "react";
-import { Link } from "react-router-dom";
 import queryString from "query-string";
-import { connect } from "react-redux";
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Link, useLocation, useHistory } from "react-router-dom";
 import { fetchItems } from "../item.actions";
 import { getBrands } from "../../../brand/client/brand.actions";
 import { getCategories } from "../../../category/client/category.actions";
 import { Label, Form, Button, Card, Divider, Icon, Menu, Container, Image, Segment, Header } from "semantic-ui-react";
 
-class ItemList extends React.Component {
-    constructor(props) {
-        super();
-        props.getBrands();
-        props.getCategories();
-        props.fetchItems(props.location.search);
+export default function ItemList() {
+    const dispatch = useDispatch();
+    const history = useHistory();
+    const location = useLocation();
 
-        this.state = {
-            params: queryString.parse(props.location.search)
-        };
+    useEffect(() => {
+        dispatch(getBrands());
+        dispatch(getCategories());
+    }, []);
 
-        this.state = {
-            ...this.state,
-            brandName: "",
-            categoryName: "",
-            brandId: this.state.params.brandId || "-1",
-            categoryId: this.state.params.categoryId || "-1"
-        };
+    useEffect(() => {
+        dispatch(fetchItems(location.search));
+    }, [location.search]);
 
-        this.filter = this.filter.bind(this);
-        this.resetFilter = this.resetFilter.bind(this);
-        this.discardFilter = this.discardFilter.bind(this);
-        this.handleInputChange = this.handleInputChange.bind(this);
-    }
+    const params = queryString.parse(location.search);
 
-    componentDidUpdate(prevProps) {
-        const params = queryString.parse(this.props.location.search);
+    const [brandId, setBrandId] = useState(params.brandId ? params.brandId : "");
+    const [categoryId, setCategoryId] = useState(params.categoryId ? params.categoryId : "");
+    const [startDate, setStartDate] = useState(params.startDate ? params.startDate : "");
+    const [endDate, setEndDate] = useState(params.endDate ? params.endDate : "");
 
-        if(this.props.categories
-            && this.props.categories.length
-            && params.categoryId
-            && params.categoryId !== "-1") {
-            this.state.categoryName = this.props.categories.find(x => x._id === params.categoryId).name;
-        }
-
-        if(this.props.brands
-            && this.props.brands.length
-            && params.brandId
-            && params.brandId !== "-1") {
-            this.state.brandName = this.props.brands.find(x => x._id === params.brandId).name;
-        }
-
-        if(this.props.location.search !== prevProps.location.search) {
-            this.props.fetchItems(this.props.location.search);
-        }
-    }
-
-    handleInputChange(event, data) {
-        this.setState({
-            [data.name]: data.value
-        });
-    }
-
-    resetFilter() {
-        this.state.brandId = "-1";
-        this.state.categoryId = "-1";
-        this.filter();
-    }
-
-    discardFilter(event) {
-        this.state[event.target.dataset.id] = "-1";
-        this.filter();
-    }
-
-    filter() {
-        if(this.state.categoryId === "-1" && this.state.brandId === "-1") {
-            this.props.history.push({ pathname: "items" });
-            return;
-        }
-
+    function filter(e, discard) {
         let q = {};
 
-        if(this.state.categoryId !== "-1") {
-            q.categoryId = this.state.categoryId;
+        if(categoryId && discard !== "category") {
+            q.categoryId = categoryId;
         }
 
-        if(this.state.brandId !== "-1") {
-            q.brandId = this.state.brandId;
+        if(brandId && discard !== "brand") {
+            q.brandId = brandId;
         }
 
-        if(this.props.location.search === `?${queryString.stringify(q)}`) return;
+        if(startDate && endDate && discard !== "date") {
+            q.startDate = startDate;
+            q.endDate = endDate;
+        }
 
-        this.props.history.push({
+        if(location.search === `?${queryString.stringify(q)}`) return;
+
+        history.push({
             pathname: "items",
             search: queryString.stringify(q)
         });
     }
 
-    render() {
-        const { categoryId, categoryName, brandId, brandName } = this.state;
-        const params = queryString.parse(this.props.location.search);
+    function resetFilter() {
+        setBrandId("");
+        setCategoryId("");
+        setStartDate("");
+        setEndDate("");
 
-        const {
-            items: { data, pagination } = items,
-            location, brands, categories
-        } = this.props;
-
-        let categoryOptions = categories.map(function(option) {
-            return { key: option._id, value: option._id, text: option.name };
-        });
-
-        let brandOptions = brands.map(function(option) {
-            return { key: option._id, value: option._id, text: option.name };
-        });
-
-        brandOptions.unshift({ key: "-1", value: "-1", text: "Select Brand" });
-        categoryOptions.unshift({ key: "-1", value: "-1", text: "Select Category" });
-
-        let cards = data.map(function(item) {
-            let activeImage = item.files.filter(x => x.active)[0];
-            activeImage = activeImage ? activeImage.secure_url : null;
-
-            return (
-                <Link key={item._id} className="ui raised card" to={`/items/${item._id}`}>
-                    <Card.Content header={item.name} className="ui center aligned"/>
-                    <Card.Content className="ui center aligned image-wrapper">
-                        { activeImage
-                            ? <Image src={activeImage} alt={item.name}/>
-                            : "Image Not Available!"
-                        }
-                    </Card.Content>
-                </Link>
-            );
-        });
-
-        let paginationLinks = [];
-
-        for(let idx = 1; idx <= pagination.pages; idx++) {
-            paginationLinks.push({
-                idx,
-                link: {
-                    pathname: location.pathname,
-                    search: queryString.stringify({ ...params, page: idx })
-                },
-                isActive: !params.page && idx === 1 || +params.page === idx
-            });
+        if(params.categoryId || params.brandId || params.startDate || params.endDate) {
+            history.push({ pathname: "items" });
         }
+    }
 
-        paginationLinks = paginationLinks.map(function(page) {
-            return (
-                <Menu.Item active={page.isActive} key={page.idx}>
-                    <Link to={page.link}>{page.idx}</Link>
-                </Menu.Item>
-            );
-        });
+    let brandName = null;
+    let categoryName = null;
+    let dateRange = null;
+
+    const {data, pagination} = useSelector(state => state.itemReducer.items);
+    const categories = useSelector(state => state.categoryReducer.categories);
+    const brands = useSelector(state => state.brandReducer.brands);
+
+    if(params.categoryId && categories && categories.length) {
+        categoryName = categories.find(x => x._id === params.categoryId).name;
+    }
+
+    if(params.brandId && brands && brands.length) {
+        brandName = brands.find(x => x._id === params.brandId).name;
+    }
+
+    if(params.startDate && params.endDate) {
+        dateRange = `${startDate}-${endDate}`;
+    }
+
+    const categoryOptions = categories.map(function(option) {
+        return { key: option._id, value: option._id, text: option.name };
+    });
+
+    const brandOptions = brands.map(function(option) {
+        return { key: option._id, value: option._id, text: option.name };
+    });
+
+    const cards = data.map(function(item) {
+        let activeImage = item.files.filter(x => x.active)[0];
+        activeImage = activeImage ? activeImage.secure_url : null;
 
         return (
-            <>
-                <Divider hidden/>
+            <Link key={item._id} className="ui raised card" to={`/items/${item._id}`}>
+                <Card.Content header={item.name} className="ui center aligned"/>
+                <Card.Content className="ui center aligned image-wrapper">
+                    { activeImage
+                        ? <Image src={activeImage} alt={item.name}/>
+                        : "Image Not Available!"
+                    }
+                </Card.Content>
+            </Link>
+        );
+    });
 
-                { cards.length > 0 &&
-                    <Segment raised className="stacked">
-                        <Header as="h2">
-                            <Icon name="archive" circular/>
-                            <Header.Content>
-                                Total Gadgets: {pagination.count}
-                                <Header.Subheader>Filter your gadgets by category, brand and price <Icon name="angle double down"></Icon></Header.Subheader>
-                            </Header.Content>
-                        </Header>
+    let paginationLinks = [];
 
-                        <Form onSubmit={this.filter}>
-                            <Form.Group inline>
-                                <Form.Dropdown
-                                    selection
+    for(let idx = 1; idx <= pagination.pages; idx++) {
+        paginationLinks.push({
+            idx,
+            link: {
+                pathname: location.pathname,
+                search: queryString.stringify({ ...params, page: idx })
+            },
+            isActive: !params.page && idx === 1 || +params.page === idx
+        });
+    }
+
+    paginationLinks = paginationLinks.map(function(page) {
+        return (
+            <Menu.Item active={page.isActive} key={page.idx}>
+                <Link to={page.link}>{page.idx}</Link>
+            </Menu.Item>
+        );
+    });
+
+    return (
+        <>
+            <Divider hidden/>
+
+            { cards.length > 0 &&
+                <Segment raised className="stacked">
+                    <Header as="h2">
+                        <Icon name="archive" circular/>
+                        <Header.Content>
+                            Total Gadgets: {pagination.count}
+                            <Header.Subheader>Filter your gadgets by category, brand and date range <Icon name="angle double down"></Icon></Header.Subheader>
+                        </Header.Content>
+                    </Header>
+
+                    <Form onSubmit={filter}>
+                        <Form.Group widths="equal">
+                            <Form.Field>
+                                <label>Gadget Category</label>
+                                <Form.Select
+                                    placeholder="Choose an option"
                                     name="categoryId"
                                     options={categoryOptions}
                                     value={categoryId}
-                                    onChange={this.handleInputChange}
+                                    onChange={(event, data) => {setCategoryId(data.value)}}
                                 />
-                                <Form.Dropdown
-                                    selection
+                            </Form.Field>
+
+                            <Form.Field>
+                                <label>Gadget Brand</label>
+                                <Form.Select
+                                    placeholder="Choose an option"
                                     name="brandId"
                                     options={brandOptions}
                                     value={brandId}
-                                    onChange={this.handleInputChange}
+                                    onChange={(event, data) => {setBrandId(data.value)}}
                                 />
+                            </Form.Field>
 
-                                <Button.Group>
-                                    <Button type="submit" positive>
-                                        <Icon name="filter"/> Filter
-                                    </Button>
-                                    <Button.Or />
-                                    <Button type="button" onClick={this.resetFilter}>
-                                        <Icon name="undo"/> Reset
-                                    </Button>
-                                </Button.Group>
-                            </Form.Group>
-                        </Form>
-                    </Segment>
-                }
+                            <Form.Field>
+                                <label>Start Date</label>
+                                <Form.Input
+                                    type="date"
+                                    placeholder="Choose a date"
+                                    name="startDate"
+                                    value={startDate}
+                                    onChange={(event, data) => {setStartDate(data.value)}}
+                                />
+                            </Form.Field>
 
-                {(params.categoryId || params.brandId) &&
-                    <div>
-                        {(params.categoryId && params.categoryId !== "-1") &&
-                            <Label color="blue">
-                                Category: {categoryName} <Icon name="delete" data-id="categoryId" onClick={this.discardFilter}/>
-                            </Label>
-                        }
+                            <Form.Field>
+                                <label>End Date</label>
+                                <Form.Input
+                                    type="date"
+                                    placeholder="Choose a date"
+                                    name="endDate"
+                                    value={endDate}
+                                    onChange={(event, data) => {setEndDate(data.value)}}
+                                />
+                            </Form.Field>
+                        </Form.Group>
 
-                        {(params.brandId && params.brandId !== "-1") &&
-                            <Label color="blue">
-                                Brand: {brandName} <Icon name="delete" data-id="brandId" onClick={this.discardFilter}/>
-                            </Label>
-                        }
+                        <Button.Group>
+                            <Button type="submit" positive>
+                                <Icon name="filter"/> Filter
+                            </Button>
+                            <Button.Or />
+                            <Button type="button" onClick={resetFilter}>
+                                <Icon name="undo"/> Reset
+                            </Button>
+                        </Button.Group>
+                    </Form>
+                </Segment>
+            }
 
-                        <Divider hidden/>
-                    </div>
-                }
+            {categoryName &&
+                <Label color="blue">
+                    Category: {categoryName} <Icon name="delete" onClick={(e) => { setCategoryId(""); filter(e, "category"); }}/>
+                </Label>
+            }
 
-                { cards.length > 0 &&
-                    <div id="item-cards-container">
-                        <Card.Group itemsPerRow={5} stackable>
-                            {cards}
-                        </Card.Group>
+            {brandName &&
+                <Label color="blue">
+                    Brand: {brandName} <Icon name="delete" onClick={(e) => { setBrandId(""); filter(e, "brand"); }}/>
+                </Label>
+            }
 
-                        { pagination.pages !== 1 &&
-                            <div>
-                                <Divider hidden/>
-                                <Container textAlign="center">
-                                    <Menu pagination>
-                                        {paginationLinks}
-                                    </Menu>
-                                </Container>
-                            </div>
-                        }
-                    </div>
-                }
-                { cards.length === 0 &&
-                    <Segment placeholder raised>
-                        <Header icon>
-                            <Icon name="warning sign" />
-                            You don't have any items!
-                        </Header>
-                        <Button primary>
-                            <Link to="/items/add" style={{color: "white"}}>Add New Item</Link>
-                        </Button>
-                    </Segment>
-                }
-            </>
-        );
-    }
+            {dateRange &&
+                <Label color="blue">
+                    Date Range: {dateRange} <Icon name="delete" onClick={(e) => { setStartDate(""); setEndDate(""); filter(e, "date"); }}/>
+                </Label>
+            }
+
+            <Divider hidden/>
+
+            { cards.length > 0 &&
+                <div id="item-cards-container">
+                    <Card.Group itemsPerRow={5} stackable>
+                        {cards}
+                    </Card.Group>
+
+                    { pagination.pages !== 1 &&
+                        <div>
+                            <Divider hidden/>
+                            <Container textAlign="center">
+                                <Menu pagination>
+                                    {paginationLinks}
+                                </Menu>
+                            </Container>
+                        </div>
+                    }
+                </div>
+            }
+
+            { cards.length === 0 &&
+                <Segment placeholder raised>
+                    <Header icon>
+                        <Icon name="warning sign" />
+                        You don't have any items!
+                    </Header>
+                    <Button primary>
+                        <Link to="/items/add" style={{color: "white"}}>Add New Item</Link>
+                    </Button>
+                </Segment>
+            }
+        </>
+    );
 }
-
-const mapStateToProps = (state, props) => {
-    return {
-        location: props.location,
-        items: state.itemReducer.items,
-        brands: state.brandReducer.brands,
-        categories: state.categoryReducer.categories
-    };
-}
-
-const mapDispatchToProps = (dispatch) => {
-    return {
-        getBrands: () => dispatch(getBrands()),
-        getCategories: () => dispatch(getCategories()),
-        fetchItems: query => dispatch(fetchItems(query))
-    };
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(ItemList);
