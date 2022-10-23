f71c116c59339a377d291d3c17c90b6a4a6db1e8
Shibbir Ahmed <shibbir.io@outlook.com>
2022-10-23 14:03:51 +0200

    Release v0.16.3

--- src/config/client/webpack.common.js
+++ src/config/client/webpack.common.js
@@ -54,11 +54,11 @@ module.exports = {
             },
             {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: "asset/resource"
+                type: "asset/inline"
             },
             {
                 test: /\.(woff|woff2|eot|ttf|otf)$/i,
-                type: "asset/resource"
+                type: "asset/inline"
             }
         ]
     }
